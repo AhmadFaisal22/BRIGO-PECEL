5c1eeb4a12197af0afc5436a5154d56f89c65bd5
Ahmad Faisal <faisal.dinus22@gmail.com>
2020-05-07 23:00:52 +0700

    modif 7

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -13,12 +13,33 @@ class Menu extends CI_Controller {
             $data['title'] = 'ATM';
             $data['user'] = $this->db->get_where('user',['email'=>
             $this->session->userdata('email')])->row_array();
-       
+
+            $data['atm']= $this->db->get('atm')->result_array();
+            
+            $this->form_validation->set_rules('tid','TID','required');
+            $this->form_validation->set_rules('description','Description','required');
+            $this->form_validation->set_rules('tanggal','Tanggal','required');
+            $this->form_validation->set_rules('time','Waktu','required');
+            
+            if ($this->form_validation->run() == false) {
                 $this->load->view('templates/header', $data);
                 $this->load->view('templates/sidebar', $data);
                 $this->load->view('templates/topbar', $data);
                 $this->load->view('menu/index', $data);
                 $this->load->view('templates/footer');
+            }else{
+                $data = [
+                    'tid' => $this->input->post('tid'),
+                    'description' => $this->input->post('description'),
+                    'tanggal' => $this->input->post('tanggal'),
+                    'time' => $this->input->post('time')
+                ];
+                $this->db->insert('atm',$data);
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+                Data Berhasil Ditambahkan 
+                </div>');
+            redirect('menu','refresh');
+            }
         
         }
         public function crm()
--- application/views/menu/edc.php
+++ application/views/menu/edc.php
@@ -1,18 +1,21 @@
   <!-- Begin Page Content -->
   <div class="container-fluid">
-
+   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+    <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
 <!-- Page Heading -->
  <!-- Begin Page Content -->
  <div class="container-fluid">
 
 <!-- Page Heading -->
 <h1 class="h3 mb-2 text-gray-800"><?=$title;?></h1>
-<p class="mb-4">Automated Teller Machine atau Automatic Teller Machine</p>
+<p class="mb-4">Electronic Data Capture</p>
 
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
   <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary">DataTables ATM Error</h6>
+    <h6 class="m-0 font-weight-bold text-primary">DataTables EDC Error</h6>
   </div>
   <div class="card-body">
     <div class="table-responsive">
@@ -23,7 +26,7 @@
             <th>No.</th>
             <th>TID</th>
             <th>Deskripsi</th>
-            <th>Tanggal</th>
+            <th>Tanggal</th><br>
             <th>Waktu</th>
             <th>Aksi</th>
           </tr>
